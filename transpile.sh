6fd32c9eafee2b9cb29d91159b49647be6582937
Joris Scheppers <joris.scheppers@luminis.eu>
2021-11-23 16:01:42 +0100

    transpile in loop

--- transpile.sh
+++ transpile.sh
@@ -1,2 +1,9 @@
-ct -pretty -strict -in-file container-linux-configs/c0-3f-d5-64-fd-54-pxe.conf -out-file ignition-configs/c0-3f-d5-64-fd-54-pxe.ign 
-ct -pretty -strict -in-file container-linux-configs/c0-3f-d5-64-fd-54-disk.conf -out-file ignition-configs/c0-3f-d5-64-fd-54-disk.ign 
+cd container-linux-configs/
+for f in *.conf 
+    do
+    echo "Processing $f"
+    ignfile=${f%%.conf*}
+    ct -pretty -strict -in-file ${f} -out-file ../ignition-configs/${ignfile}.ign 
+done
+#ct -pretty -strict -in-file container-linux-configs/c0-3f-d5-64-fd-54-pxe.conf -out-file ignition-configs/c0-3f-d5-64-fd-54-pxe.ign 
+#ct -pretty -strict -in-file container-linux-configs/c0-3f-d5-64-fd-54-disk.conf -out-file ignition-configs/c0-3f-d5-64-fd-54-disk.ign 
