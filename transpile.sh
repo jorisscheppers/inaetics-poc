340484e40f6387548cd29ad39433d1d387b1a363
Joris Scheppers <joris.scheppers@luminis.eu>
2021-11-30 15:39:16 +0100

    remove strict option

--- transpile.sh
+++ transpile.sh
@@ -3,7 +3,5 @@ for f in *.conf
     do
     echo "Processing $f"
     ignfile=${f%%.conf*}
-    ct -pretty -strict -in-file ${f} -out-file ../ignition-configs/${ignfile}.ign 
+    ct -pretty -in-file ${f} -out-file ../ignition-configs/${ignfile}.ign #-strict 
 done
-#ct -pretty -strict -in-file container-linux-configs/c0-3f-d5-64-fd-54-pxe.conf -out-file ignition-configs/c0-3f-d5-64-fd-54-pxe.ign 
-#ct -pretty -strict -in-file container-linux-configs/c0-3f-d5-64-fd-54-disk.conf -out-file ignition-configs/c0-3f-d5-64-fd-54-disk.ign 
