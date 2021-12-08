a4e874b287ad64221c287a473e38246ecb09fce6
Joris Scheppers <joris.scheppers@luminis.eu>
2021-12-08 10:53:54 +0100

    Integrate scripts

--- scripts/kubernetes/kubernetes-controller/generate-worker-config.bash
+++ scripts/kubernetes/kubernetes-controller/generate-worker-config.bash
@@ -1,13 +1,16 @@
+#WORKER JOIN CONFIG
+join_token="inaeti.csjointoken12345"
 URL=$(kubectl config view -ojsonpath='{.clusters[0].cluster.server}')
 prefix="https://"
 short_url=${URL#"$prefix"}
+
 cat <<EOF
 apiVersion: kubeadm.k8s.io/v1beta2
 kind: JoinConfiguration
 discovery:
   bootstrapToken:
     apiServerEndpoint: $short_url
-    token: $(kubeadm token create)
+    token: $(kubeadm token create $join_token)
     caCertHashes:
     - sha256:$(openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //')
 controlPlane:
--- scripts/kubernetes/kubernetes-controller/initialise-controller.bash
+++ scripts/kubernetes/kubernetes-controller/initialise-controller.bash
@@ -1,3 +1,28 @@
+#KUBEADM
+cat <<EOF | tee kubeadm-config.yaml
+apiVersion: kubeadm.k8s.io/v1beta2
+kind: InitConfiguration
+nodeRegistration:
+  kubeletExtraArgs:
+    volume-plugin-dir: "/opt/libexec/kubernetes/kubelet-plugins/volume/exec/"
+---
+apiVersion: kubeadm.k8s.io/v1beta2
+kind: ClusterConfiguration
+networking:
+  podSubnet: 192.168.0.0/16
+controllerManager:
+  extraArgs:
+    flex-volume-plugin-dir: "/opt/libexec/kubernetes/kubelet-plugins/volume/exec/"
+EOF
+
+kubeadm config images pull
+kubeadm init --config kubeadm-config.yaml
+
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+
+
+#CALICO CNI
 cat <<EOF | tee calico.yaml
 # Source: https://docs.projectcalico.org/manifests/custom-resources.yaml
 apiVersion: operator.tigera.io/v1
