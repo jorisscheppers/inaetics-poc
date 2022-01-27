01da13621e647f54fceeb4e62684b01425e96d48
Joris Scheppers <joris.scheppers@luminis.eu>
2022-01-27 13:38:08 +0100

    changed signature of curl command use

--- scripts/tftp/tftp.bash
+++ scripts/tftp/tftp.bash
@@ -2,7 +2,7 @@
 
 set -xe
 
-curl() {
+curlo() {
 	command curl -sSLO "$@"
 }
 
@@ -13,28 +13,26 @@ mkdir /var/ftpd/flatcar_production
 
 #root files
 cd /var/ftpd
-curl https://github.com/jorisscheppers/inaetics-poc/blob/4db59064f1efdd18a742ed522f87fc0f916af61a/PXE/ldlinux.c32
-curl https://github.com/jorisscheppers/inaetics-poc/blob/4db59064f1efdd18a742ed522f87fc0f916af61a/PXE/menu.c32
-curl https://github.com/jorisscheppers/inaetics-poc/blob/4db59064f1efdd18a742ed522f87fc0f916af61a/PXE/pxelinux.0
+curlo https://github.com/jorisscheppers/inaetics-poc/blob/4db59064f1efdd18a742ed522f87fc0f916af61a/PXE/ldlinux.c32
+curlo https://github.com/jorisscheppers/inaetics-poc/blob/4db59064f1efdd18a742ed522f87fc0f916af61a/PXE/menu.c32
+curlo https://github.com/jorisscheppers/inaetics-poc/blob/4db59064f1efdd18a742ed522f87fc0f916af61a/PXE/pxelinux.0
 
 #pxelinux config files for each known node
 cd /var/ftpd/pxelinux.cfg
-curl https://raw.githubusercontent.com/jorisscheppers/inaetics-poc/main/PXE/pxelinux.cfg/01-c0-3f-d5-64-fd-54
-curl https://raw.githubusercontent.com/jorisscheppers/inaetics-poc/main/PXE/pxelinux.cfg/01-c0-3f-d5-67-01-ab
-curl https://raw.githubusercontent.com/jorisscheppers/inaetics-poc/main/PXE/pxelinux.cfg/01-c0-3f-d5-67-02-9f
+curlo https://raw.githubusercontent.com/jorisscheppers/inaetics-poc/main/PXE/pxelinux.cfg/01-c0-3f-d5-64-fd-54
+curlo https://raw.githubusercontent.com/jorisscheppers/inaetics-poc/main/PXE/pxelinux.cfg/01-c0-3f-d5-67-01-ab
+curlo https://raw.githubusercontent.com/jorisscheppers/inaetics-poc/main/PXE/pxelinux.cfg/01-c0-3f-d5-67-02-9f
 
 #memdisk files
-cd /var/ftpd/memdisk
-curl https://github.com/jorisscheppers/inaetics-poc/raw/79259e8c64cdff255bf430234ac622a9424b5ca4/PXE/memdisk.tar
-tar -xzf memdisk.tar
+cd /var/ftpd/
+curl -sSL https://github.com/jorisscheppers/inaetics-poc/raw/79259e8c64cdff255bf430234ac622a9424b5ca4/PXE/memdisk.tar tar -C /var/ftpd/memdisk -xz
 
 #flatcar linux latest stable release
 cd /var/ftpd/flatcar_production
-curl https://stable.release.flatcar-linux.net/amd64-usr/current/flatcar_production_pxe_image.cpio.gz
-curl https://stable.release.flatcar-linux.net/amd64-usr/current/flatcar_production_pxe_grub.efi
-curl https://stable.release.flatcar-linux.net/amd64-usr/current/flatcar_production_pxe.vmlinuz
-curl https://stable.release.flatcar-linux.net/amd64-usr/current/flatcar_production_pxe.sh
-
+curlo https://stable.release.flatcar-linux.net/amd64-usr/current/flatcar_production_pxe_image.cpio.gz
+curlo https://stable.release.flatcar-linux.net/amd64-usr/current/flatcar_production_pxe_grub.efi
+curlo https://stable.release.flatcar-linux.net/amd64-usr/current/flatcar_production_pxe.vmlinuz
+curlo https://stable.release.flatcar-linux.net/amd64-usr/current/flatcar_production_pxe.sh
 
 
 #download and enable dnsmasq
