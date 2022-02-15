b6e5b72b06754c55cbcdae6389a5edf5021dbe32
Joris Scheppers <joris.scheppers@luminis.eu>
2022-02-15 10:28:39 +0100

    Expanded port range

--- scripts/k8s/init-k8s.bash
+++ scripts/k8s/init-k8s.bash
@@ -29,7 +29,7 @@ apiVersion: kubeadm.k8s.io/v1beta3
 kind: ClusterConfiguration
 apiServer:
   extraArgs:
-    service-node-port-range: 25000-35000
+    service-node-port-range: 5000-35000
 networking:
   podSubnet: 192.168.0.0/16
 controllerManager:
