c60d9bef09cd302eb069108995cf2ca98f65d77a
Joris Scheppers <joris.scheppers@luminis.eu>
2022-02-17 13:52:58 +0100

    Keep it simple

--- scripts/ig/install-ig.bash
+++ scripts/ig/install-ig.bash
@@ -2,9 +2,9 @@
 
 set -xe
 
-curl http://10.0.6.12:8080/exports.bash | tee /configs/secrets/exports.bash
+curl http://10.0.6.12:8080/exports.bash | tee /configs/exports.bash
 
-source /configs/secrets/exports.bash
+source /configs/exports.bash
 
 flux bootstrap github --owner jorisscheppers --repository=inaetics-poc --branch=main --path=clusters/infra --personal
 
