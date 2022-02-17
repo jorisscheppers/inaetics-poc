2a728c3a504486d2324f1c9ad2c8bfdb7aef29b3
Joris Scheppers <joris.scheppers@luminis.eu>
2022-02-17 13:33:10 +0100

    Add HTTP server for exports

--- scripts/tftp/tftp.bash
+++ scripts/tftp/tftp.bash
@@ -14,6 +14,8 @@ mkdir /var/ftpd
 mkdir /var/ftpd/pxelinux.cfg
 mkdir /var/ftpd/flatcar_production
 
+mkdir /share
+
 #pxelinux config files for each known node
 cd /var/ftpd/pxelinux.cfg
 curlo https://raw.githubusercontent.com/jorisscheppers/inaetics-poc/main/PXE/pxelinux.cfg/01-c0-3f-d5-64-fd-54
@@ -29,3 +31,9 @@ curlo https://stable.release.flatcar-linux.net/amd64-usr/current/flatcar_product
 
 #run Docker container for TFTP service
 docker run -d -p 69:1069/udp --name tftp-container --env TFTPD_EXTRA_ARGS="--blocksize 1468" --mount type=bind,source=/var/ftpd/flatcar_production,target=/tftpboot/boot --mount type=bind,source=/var/ftpd/pxelinux.cfg,target=/tftpboot/pxelinux.cfg kalaksi/tftpd 
+
+#MANUAL STEP:
+# copy exports.bash to tftp server, folder /share
+
+#run nginx container for exports file
+docker run -p8080:80 --name httpshare -v /share:/usr/share/nginx/html:ro -d nginx
