1767173280c66922134784cea7aeff38d534b63b
Joris Scheppers <joris.scheppers@luminis.eu>
2022-02-18 12:40:52 +0100

    Reflect changes in container port setup

--- scripts/ig/install-ig.bash
+++ scripts/ig/install-ig.bash
@@ -5,11 +5,13 @@ set -xe
 DOWNLOAD_DIR=/opt/bin
 export PATH=$PATH:$DOWNLOAD_DIR
 
-curl http://10.0.6.12:8080/exports.bash | tee /configs/exports.bash
+curl http://10.0.60.3:8000/exports.bash | tee /configs/exports.bash
 
 source /configs/exports.bash
 
 flux bootstrap github --owner jorisscheppers --repository=inaetics-poc --branch=main --path=clusters/infra --personal
+
+
 
 kubectl -n informationgrid create secret docker-registry informationgrid-pullsecret \
     --docker-server=informationgrid-docker.luminis.net \
