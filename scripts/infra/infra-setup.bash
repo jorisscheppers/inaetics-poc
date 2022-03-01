2d192175751be18b2a1b373274bede5db3594a21
Joris Scheppers <joris.scheppers@luminis.eu>
2022-03-01 13:46:09 +0100

    Move to infra folder

--- scripts/infra/infra-setup.bash
+++ scripts/infra/infra-setup.bash
@@ -2,6 +2,7 @@
 set -xe
 
 #create sources dir and clone Git repo for all scripts
+sudo rm -rf /sources
 sudo mkdir /sources
 cd /sources
 sudo git clone https://github.com/jorisscheppers/inaetics-poc.git
