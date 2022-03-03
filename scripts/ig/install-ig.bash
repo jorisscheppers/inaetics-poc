4eb7193bfabf2c6c9eb3aa9343b86fa00794750a
Joris Scheppers <joris.scheppers@luminis.eu>
2022-03-03 12:22:05 +0100

    Install from local helm charts

--- scripts/ig/install-ig.bash
+++ scripts/ig/install-ig.bash
@@ -13,9 +13,9 @@ kubectl apply -f http://infra.cluster.local/clusters/infra/informationgrid-ns.ya
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
-helm -n informationgrid install mongodb bitnami/mongodb --version 10.28.7 --values http://infra.cluster.local/clusters/infra/mongodb-values.yaml
-helm -n informationgrid install kafka bitnami/kafka --version 14.2.5 --values http://infra.cluster.local/clusters/infra/kafka-values.yaml
-helm -n informationgrid install elasticsearch bitnami/elasticsearch --version 17.1.1 --values http://infra.cluster.local/clusters/infra/elasticsearch-values.yaml
+helm -n informationgrid install mongodb http://infra.cluster.local/archives/mongodb-10.28.7.tgz --values http://infra.cluster.local/clusters/infra/mongodb-values.yaml
+helm -n informationgrid install kafka http://infra.cluster.local/archives/kafka-14.2.5.tgz --values http://infra.cluster.local/clusters/infra/kafka-values.yaml
+helm -n informationgrid install elasticsearch http://infra.cluster.local/archives/elasticsearch-17.1.1.tgz --values http://infra.cluster.local/clusters/infra/elasticsearch-values.yaml
 
 kubectl apply -f http://infra.cluster.local/clusters/infra/mongodb-nodeport.yaml
 
