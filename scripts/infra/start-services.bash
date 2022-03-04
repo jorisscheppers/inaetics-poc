89bf4842dd234d383f88edf3490bb6162d60e564
Joris Scheppers <joris.scheppers@luminis.eu>
2022-03-04 14:14:26 +0100

    Splitting not a good idea

--- scripts/infra/start-services.bash
+++ scripts/infra/start-services.bash
@@ -1,6 +1,33 @@
 #!/bin/bash
 set -xe
 
+#remove shared folders
+sudo rm -rf /var/ftpd/pxelinux.cfg
+sudo rm -rf /share/*
+
+
+#create http shared dir
+sudo mkdir -p /share
+sudo mkdir -p /share/secrets
+
+#(re)create configs folder
+
+#copy relevant sources to destination dirs
+#pxelinux config files for each known node
+sudo cp -r /sources/inaetics-poc/PXE/pxelinux.cfg /var/ftpd/
+#ignition configs for each known node
+sudo cp -r /sources/inaetics-poc/ignition-configs /share
+#all scripts
+sudo cp -r /sources/inaetics-poc/scripts /share
+#dnsmasq.conf
+sudo cp -r /sources/inaetics-poc/configs /share
+#copy cluster info to /share
+sudo cp -r /sources/inaetics-poc/clusters /share
+#copy archives to /share
+sudo cp -r /sources/inaetics-poc/archives /share
+#copy exports
+sudo cp /sources/exports.bash /share/secrets
+
 #stop and remove all running containers
 for container_id in $(docker ps -q); do docker stop $container_id; done
 for container_id in $(docker ps -a -q); do docker rm $container_id; done
