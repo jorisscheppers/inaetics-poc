8575f2d88e3ab9b8756fad07cd6f80fed4209545
Joris Scheppers <joris.scheppers@luminis.eu>
2022-03-15 12:17:32 +0100

    how did I not notice this typo?

--- scripts/infra/flatcar-download.bash
+++ scripts/infra/flatcar-download.bash
@@ -12,7 +12,7 @@ sudo mkdir -p /var/ftpd/flatcar_production
 
 #flatcar linux latest stable release
 cd /var/ftpd/flatcar_production
-curlo https://stable.release.flatcar-linux.net/amd64-usr/current/flatcar_production_pxe_image.cpio.g
+curlo https://stable.release.flatcar-linux.net/amd64-usr/current/flatcar_production_pxe_image.cpio.gz
 curlo https://stable.release.flatcar-linux.net/amd64-usr/current/flatcar_production_pxe_grub.efi
 curlo https://stable.release.flatcar-linux.net/amd64-usr/current/flatcar_production_pxe.vmlinuz
 curlo https://stable.release.flatcar-linux.net/amd64-usr/current/flatcar_production_pxe.sh
