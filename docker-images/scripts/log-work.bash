0b5baa4389c2f6a18a290715e82c1ecb9329380d
Joris Scheppers <joris.scheppers@luminis.eu>
2022-03-15 14:36:35 +0100

    Made IG endpoint configurable

--- docker-images/scripts/log-work.bash
+++ docker-images/scripts/log-work.bash
@@ -1,8 +1,9 @@
 x=1
+echo 'CURLing to '"$IG_ENDPOINT"
 while true
 do
   DATE=$(date +"%Y-%m-%dT%TZ")
-  curl -X POST "http://node1.cluster.local:8081/ig/rest/ig-document-service/collection/records" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"time\":\"$DATE\",\"value\":\"Iteration: $x\"}" -H "Authorization: Bearer ozZNC7gHChGzOaIrI1KZTA=="
+  curl -X POST 'http://'"$IG_ENDPOINT"'/ig/rest/ig-document-service/collection/records' -H "accept: application/json" -H "Content-Type: application/json" -d "{\"time\":\"$DATE\",\"value\":\"Iteration: $x\"}" -H "Authorization: Bearer ozZNC7gHChGzOaIrI1KZTA=="
   x=$(( $x + 1 ))
   echo 
   sleep 10s
